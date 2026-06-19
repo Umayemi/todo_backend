900f8c48393899c2c5bb9a02d00c3d7972f3adb3
umayemi <adeniranuma@yahoo.com>
2026-06-19 19:31:37 +0100

    fixed cors issue

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import routes from "./todos.route";
+import cors from 'cors';
 
 
 const app = express();
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+    ],
+    credentials: true,
+  })
+);
 app.use(routes);
 
 
